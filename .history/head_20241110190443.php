d97288f54b42082f0c79e2f3b73d39c4bbe5bf6e
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2024-11-10 19:10:10 +0100

    mise en place environnemment de trvail

---
+++ .history/head_20241110190443.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <!-- Meta Tags -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta property="og:title" content="Harmony Digital - Experts en Création de Sites Web et Marketing Digital">
+  <meta property="og:description" content="Harmony Digital : Conception de sites internet, design de maquettes et logos, SEO, marketing digital, communication événementielle et campagnes publicitaires. Services sur mesure pour booster votre visibilité en ligne.">
+  <meta property="og:image" content="images/logo.ico">
+  <meta name="author" content="HOFFMANN MICHEL - Harmony Digital">
+
+  <!--Balises Open Graph pour un meilleur partage sur les réseaux sociaux -->
+  <meta property="og:title" content="Agence Création de Sites Internet - HARMONY DIGITAL">
+  <meta property="og:description" content="Nous créons des sites internet performants et optimisés pour le SEO.">
+  <meta property="og:image" content="images/logo.ico">
+  <meta property="og:url" content="https://harmony-digital.fr/">
+
+
+  <!-- Favicon -->
+  <link rel="icon" href="images/logo.ico">
+
+  <!-- CSS Links -->
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Changa:wght@200..800&family=Jacquard+12&family=Orbitron:wght@400..900&family=Playfair+Display+SC:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&family=Special+Elite&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Black+And+White+Picture&display=swap" rel="stylesheet">
+
+  <!-- Scripts -->
+  <!-- <script src="https://www.google.com/recaptcha/api.js" async defer></script> -->
+
+  <!-- Google Tag Manager -->
+  <!-- <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-NCWSBWT7');
+  </script> -->
+  <!-- End Google Tag Manager -->
+
+  <!-- Google Analytics -->
+  <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-HPGMZYGZ9P"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-HPGMZYGZ9P');
+  </script> -->
+
+
+
+  <!-- css Titres ne fonctionne pas ds le fichier css-->
+  <style>
+   
+  </style>
