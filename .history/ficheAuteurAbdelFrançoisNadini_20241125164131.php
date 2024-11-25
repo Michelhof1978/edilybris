4e4c62a5cf955663f2e4282519241aac273115bd
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2024-11-25 16:48:13 +0100

    photo auiteurs index

---
+++ .history/ficheAuteurAbdelFrançoisNadini_20241125164131.php
@@ -0,0 +1,11 @@
+<?php include("head.php"); ?>
+
+ <title></title>
+ <meta name="description" content="">
+
+ <style>
+  ZZ
+
+</body>
+
+</html>
---
+++ .history/ficheAuteurAbdelFrançoisNadini_20241125164546.php
@@ -0,0 +1,18 @@
+<?php include("head.php"); ?>
+
+ <title></title>
+ <meta name="description" content="">
+
+ <style>
+  
+
+ 
+
+<?php include("footer.php"); ?>
+<script>
+
+</script>
+
+</body>
+
+</html>
