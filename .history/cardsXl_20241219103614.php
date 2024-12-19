61ee3d674a582fe69de002e0460f23e4b50bc47a
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2024-12-19 10:57:33 +0100

    defilement cards xl ok

---
+++ .history/cardsXl_20241219103614.php
@@ -0,0 +1,183 @@
+<!-- CARDS XL Mobile -->
+<div class="d-block d-md-none mb-3">
+        <!-- Groupe 1 Brandao Pierre -->
+        <div id="new-group1" class="new-card-container">
+            <div class="new-card">
+                <a href="link_to_book_A.html">
+                    <img src="images/pierreBrandao/XLlamuserie.webp" class="img-fluid" alt="Couverture de livre l'amuserie">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Brandao Pierre</div>
+                        <div class="new-card-price">15,00 €</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <!-- Groupe 2 Cathelineau Jocelyne -->
+        <div id="new-group2" class="new-card-container new-hidden">
+            <div class="new-card">
+                <a href="link_to_book_F.html">
+                    <img src="images/JocelyneCathelineau/couv-recto-mystere-hometrou.webp" class="img-fluid" alt="Couverture de livre les mystères de l'Hometrou">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Cathelineau Jocelyne</div>
+                        <div class="new-card-price">23,00 €</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <!-- Groupe 3 Chauvin Lydia-->
+        <div id="new-group3" class="new-card-container new-hidden">
+            <div class="new-card">
+                <a href="link_to_book_F.html">
+                    <img src="images/lydiaChauvin/couv-recto-recette-pour-un-spectacle.webp" class="img-fluid" alt="Couverture de livre Recette pour un spectacle">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Chauvin Lydia</div>
+                        <div class="new-card-price">12,00 €</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <!-- Groupe 4 Cretel Jean-Pierre-->
+        <div id="new-group4" class="new-card-container new-hidden">
+            <div class="new-card">
+                <a href="link_to_book_F.html">
+                    <img src="images/jeanPierreCretel/couv-recto-2_11.webp" class="img-fluid" alt="Couverture de livre Sentier de traverses">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Cretel Jean-Pierre</div>
+                        <div class="new-card-price">12,00 €</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <!-- Groupe 5 Cretel Jean-Pierre-->
+        <div id="new-group5" class="new-card-container new-hidden">
+            <div class="new-card">
+                <a href="link_to_book_F.html">
+                    <img src="images/jeanPierreCretel/parfois-les-vagues-couv.webp" class="img-fluid" alt="Couverture de livre Parfois les vagues">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Cretel Jean-Pierre</div>
+                        <div class="new-card-price">13,00 €</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <!-- Groupe 6 Galard François-->
+        <div id="new-group6" class="new-card-container new-hidden">
+            <div class="new-card">
+                <a href="link_to_book_F.html">
+                    <img src="images/francoisGalard/couvrectoluminescence.webp" class="img-fluid" alt="Couverture de livre luminescence">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Galard François</div>
+                        <div class="new-card-price">13,00 €</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <!-- Groupe 6 Galard François-->
+        <div id="new-group6" class="new-card-container new-hidden">
+            <div class="new-card">
+                <a href="link_to_book_F.html">
+                    <img src="images/francoisGalard/couvrectoluminescence.webp" class="img-fluid" alt="Couverture de livre luminescence">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Galard François</div>
+                        <div class="new-card-price">13,00 €</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        
+        <!-- Boutons de navigation -->
+        <div class="new-navigation">
+            <button id="new-mobile-backButton" class="new-button xl-button text-white" disabled>Précédent</button>
+            <button id="new-mobile-nextGroupButton" class="new-button xl-button text-white">Suivant</button>
+        </div>
+    </div>
+
+    <!-- CARDS XL DESKTOP-->
+    <div class="d-none d-md-block">
+        <!-- Groupe 1 -->
+        <div id="desktop-group1" class="new-card-container">
+            <!-- carte 1 -->
+            <div class="new-card">
+                <a href="link_to_book_A.html">
+                    <img src="images/pierreBrandao/XLlamuserie.webp" class="img-fluid" alt="Couverture de livre L'amuserie">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Brandao Pierre</div>
+                        <div class="new-card-price">15,00 €</div>
+                    </div>
+                </a>
+            </div>
+            <!-- carte 2 -->
+            <div class="new-card">
+                <a href="link_to_book_B.html">
+                    <img src="images/JocelyneCathelineau/couv-recto-mystere-hometrou.webp" class="img-fluid" alt="Couverture de livre les mystères de l'Hometrou">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Cathelineau Jocelyne</div>
+                        <div class="new-card-price">23,00 €</div>
+                    </div>
+                </a>
+            </div>
+            <!-- carte 3 -->
+            <div class="new-card">
+                <a href="link_to_book_C.html">
+                    <img src="images/lydiaChauvin/couv-recto-recette-pour-un-spectacle.webp" class="img-fluid" alt="Couverture de livre Recette pour un spectacle">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Chauvin Lydia</div>
+                        <div class="new-card-price">12,00 €</div>
+                    </div>
+                </a>
+            </div>
+            <!-- carte 4 -->
+            <div class="new-card">
+                <a href="link_to_book_D.html">
+                    <img src="images/jeanPierreCretel/parfois-les-vagues-couv.webp" class="img-fluid" alt="Couverture de livre Sentier de traverses">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Cretel Jean-Pierre</div>
+                        <div class="new-card-price">12,00 €</div>
+                    </div>
+                </a>
+            </div>
+            <!-- carte 5 -->
+            <div class="new-card">
+                <a href="link_to_book_E.html">
+                    <img src="images/jeanPierreCretel/couv-recto-2_11.webp" class="img-fluid" alt="Couverture de livre Parfois les vagues">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Cretel Jean-Pierre</div>
+                        <div class="new-card-price">13,00 €</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <!-- Groupe 2 -->
+        <div id="desktop-group2" class="new-card-container new-hidden">
+            <!-- cartes 6-10 -->
+            <div class="new-card">
+                <a href="link_to_book_F.html">
+                    <img src="images/francoisGalard/couvrectoluminescence.webp" class="img-fluid" alt="Couverture de livre luminescence">
+                    <div class="new-card-content">
+                        <div class="new-card-author">Galard François</div>
+                        <div class="new-card-price">13,00 €</div>
+                    </div>
+                </a>
+            </div>
+            <!-- Autres cartes du groupe 2 -->
+        </div>
+
+        <!-- Groupe 3 -->
+        <div id="desktop-group3" class="new-card-container new-hidden">
+            <!-- cartes 11-15 -->
+            <!-- Structure similaire aux cartes précédentes -->
+        </div>
+
+        <!-- Boutons de navigation -->
+        <div class="new-navigation">
+            <button id="new-desktop-backButton" class="new-button xl-button text-white" disabled>Précédent</button>
+            <button id="new-desktop-nextGroupButton" class="new-button xl-button text-white">Suivant</button>
+        </div>
+    </div>
