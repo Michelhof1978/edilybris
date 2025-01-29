a245f9d2287a7950ad70922f66702a946298de94
harmonydigital <michelhoff1978@gmail.com>
2025-01-29 08:53:38 +0100

    weber jannick

--- .history/ficheJannickWeberLeCarnassierSaintongeais_20250129085044.php
+++ .history/ficheJannickWeberLeCarnassierSaintongeais_20250129085044.php
@@ -43,18 +43,18 @@
                   
 <h4 class="h2Jaune text-center m-3 ">CHAPITRE 1</h4>
 
-<p class=" lead text-center">Je sais que je suis étendu sur mon pieu. C’est une certitude parce que je me souviens qu’ils m’ont soulevé à plusieurs. J’en ai vu de toutes les couleurs et j’ai bramé comme un cerf. Enfin, je pense que des sons sont sortis de mon gosier mais dans mon état, allez savoir ! Maintenant, tout va bén. J’ai l’impression que mon corps flotte au-dessus de ma paillasse. Je me sens tellement léger que j’ai l’impression d’être une ajhace[1]. Je me dirige doucement vers un tunnel gris métallisé et je goûte avec délice ces instants maghiques quand j’entends des messes basses.<br>
-— Je crois qu’il n’en a plus pour longtemps.
-Je reconnais la voix de ma dulcinée vieillissante mais pas celle qui lui répond. C’est pt’ être le draule du voisin de la ferme  des Mulots. Il habite un endroit prédestiné : rongeur campagnard, o lui va comme un gant, avec ses yeux vicieux et  ses petites dents acérées prêtes à la mordrerie de tout ce qu’il trouve. J’ignorais qu’il éprouvait de la sympathie à mon égard, quoique... ce soit plutôt de la curiosité malsaine. S’il ressemble à son père, rén d’étonnant.<br>
-– Le docteur a dit qu’il pouvait vivre encore plusieurs jours.<br>
-Si je dois vivre dans les conditions actuelles, je suis d’accord !<br>
-Leurs goules dau diablle[2] poursuivent.
-— Le pov, il a tellement souffert que je lui pardonne car il n’était pas toujours gentil.<br>
-— Cela fait combien de temps que vous êtes mariés ?<br>
-— Quarante-sept ans dans deux mois.<br>
-— C’est un bail !<br>
-— Oui, j’ai dû souvent me cramponner.<br>
-Leurs clapets se ferment. Je ne sais pas qui discute avec ma vélle et o m’tape sur la coloquinte. A-t-elle besoin, cette prune melée de raconter ma vie ! Moi aussi, je l’ai supportée mais heureusement, j’avais des compensations extérieures. La pov Nicole n’était pas une beauté et avec les années, elle a davantage ressemblé à un bonhomme Michelin qu’à une femme attirante. Il m’aurait fallu une mobylette pour en faire le tour ! Pas étonnant si j’allais voer ailleurs ! De plus, elle était bête comme trente six cochons mais, comme on dit, quand le vin est tiré, il faut le boire…</p><br>
+<p class=" lead text-center">Le mois de novembre s’annonçait particulièrement pluvieux et, en cela, collait peu ou prou à la définition étymologique du mois de brumaire, selon le calendrier républicain. Les nuages bas, qui obscurcissaient le ciel dans un voile continu, laissaient présager de nouvelles précipitations, ce qui ne changeait en rien le mauvais temps qui sévissait depuis ces derniers jours. Depuis quand pleuvait-il sur la ville ? Nul n’aurait su le dire avec précision. C’était comme au jour du jugement dernier, le ciel commençait à offrir aux regards le tumulte annonciateur d’un chaos imminent.<br>
+Les premières gouttes de pluie, chassées par un vent tourbillonnant, se mirent à fouetter violemment la fenêtre de la salle à manger. Un rideau en plastique transparent constellé de déjections de mouches, retenu sur le châssis par des punaises, occultait les vitres poussiéreuses, jamais nettoyées. Le bruit de l’eau projetée violemment sur les carreaux s’intensifiait progressivement et l’intérieur de l’appartement, simplement éclairé par une ampoule nue pendant au bout d’un fil, s’assombrit singulièrement. L’atmosphère glauque sur fond de tons grisâtres qui se dégageait de cet endroit reflétait l’apparence de tristesse, de misère et de solitude de ses occupants.  <br>
+Accroché au mur de la cuisine avec une pointe rouillée, un calendrier publicitaire de la marque « Suze », se balançait au gré des courants d’air, semblant attendre qu’on le débarrasse de sa feuille du mois pour enfin dévoiler l’image des paysages enneigés de décembre. Sur le côté, on pouvait y voir la photo d’un paysan muni d’une sorte de pic, appelé « fourche du diable » en train d’extraire les racines de la gentiane qui, macérées et distillées produisaient ce fameux apéritif au goût amer qu’est la Suze. Il s’agissait d’une reproduction en miniature de l’œuvre d’André Roz, peintre du Haut Doubs, primé pour plusieurs de ses œuvres rurales. <br>
+De petites taches de gras apparaissaient de ci, de là, montrant que l’almanach était feuilleté régulièrement. Ce jour-là, il indiquait : mardi 2 Novembre 1931, fête des défunts et de Victorin.<br>
+Encore enfoui sous l’édredon rouge délavé rempli de plumes d’oie, et peu enclin à s’extirper de cet endroit douillet, Vladimir se demanda si le père allait se souvenir du jour de son anniversaire et murmura : « Ce serait étonnant, il oublie toujours ! »<br>
+La porte d’un placard se ferma en grinçant sur ses gonds non huilés depuis des lustres. Vladimir sursauta et s’entendit interpeller :<br>
+— Dépêche-toi, tu vas être en retard à l’école … et tâche de ne pas te bagarrer.<br>
+— Ouais, P’pa ! ... Tu sais quel jour on est ?<br>
+— Mardi, pourquoi cette question ?<br>
+— Eh bien, parce que j’ai dix ans aujourd’hui.<br>
+— Ah, bon ! C‘est bien.<br>
+Ce fut tout. Le battant de la porte se referma presque aussitôt sur la respiration rauque et saccadée de Laurent Jamin, son père.</p><br>
                     <div class="text-center mt-3">
                         <a href="images/bulletin-de-commande-2024.pdf" class="btn btn-warning fw-bold text-white" download>Télécharger le bon de commande</a>
                     </div>
@@ -94,17 +94,17 @@ Leurs clapets se ferment. Je ne sais pas qui discute avec ma vélle et o m’tap
 <h4 class="h2Jaune text-center m-3 ">CHAPITRE 1</h4>
 
 
-<p class=" lead text-center">Je sais que je suis étendu sur mon pieu. C’est une certitude parce que je me souviens qu’ils m’ont soulevé à plusieurs. J’en ai vu de toutes les couleurs et j’ai bramé comme un cerf. Enfin, je pense que des sons sont sortis de mon gosier mais dans mon état, allez savoir ! Maintenant, tout va bén. J’ai l’impression que mon corps flotte au-dessus de ma paillasse. Je me sens tellement léger que j’ai l’impression d’être une ajhace[1]. Je me dirige doucement vers un tunnel gris métallisé et je goûte avec délice ces instants maghiques quand j’entends des messes basses.<br>
+<p class=" lead text-center">Je sais que je suis étendu sur mon pieu. C’est une certitude parce que je me souviens qu’ils m’ont soulevé à plusieurs. J’en ai vu de toutes les couleurs et j’ai bramé comme un cerf. Enfin, je pense que des sons sont sortis de mon gosier mais dans mon état, allez savoir ! Maintenant, tout va bén. J’ai l’impression que mon corps flotte au-dessus de ma paillasse. Je me sens tellement léger que j’ai l’impression d’être une ajhace[1]. Je me dirige doucement vers un tunnel gris métallisé et je goûte avec délice ces instants maghiques quand j’entends des messes basses.
 — Je crois qu’il n’en a plus pour longtemps.
-Je reconnais la voix de ma dulcinée vieillissante mais pas celle qui lui répond. C’est pt’ être le draule du voisin de la ferme  des Mulots. Il habite un endroit prédestiné : rongeur campagnard, o lui va comme un gant, avec ses yeux vicieux et  ses petites dents acérées prêtes à la mordrerie de tout ce qu’il trouve. J’ignorais qu’il éprouvait de la sympathie à mon égard, quoique... ce soit plutôt de la curiosité malsaine. S’il ressemble à son père, rén d’étonnant.<br>
-– Le docteur a dit qu’il pouvait vivre encore plusieurs jours.<br>
-Si je dois vivre dans les conditions actuelles, je suis d’accord !<br>
+Je reconnais la voix de ma dulcinée vieillissante mais pas celle qui lui répond. C’est pt’ être le draule du voisin de la ferme  des Mulots. Il habite un endroit prédestiné : rongeur campagnard, o lui va comme un gant, avec ses yeux vicieux et  ses petites dents acérées prêtes à la mordrerie de tout ce qu’il trouve. J’ignorais qu’il éprouvait de la sympathie à mon égard, quoique... ce soit plutôt de la curiosité malsaine. S’il ressemble à son père, rén d’étonnant.
+– Le docteur a dit qu’il pouvait vivre encore plusieurs jours.
+Si je dois vivre dans les conditions actuelles, je suis d’accord !
 Leurs goules dau diablle[2] poursuivent.
-— Le pov, il a tellement souffert que je lui pardonne car il n’était pas toujours gentil.<br>
-— Cela fait combien de temps que vous êtes mariés ?<br>
-— Quarante-sept ans dans deux mois.<br>
-— C’est un bail !<br>
-— Oui, j’ai dû souvent me cramponner.<br>
+— Le pov, il a tellement souffert que je lui pardonne car il n’était pas toujours gentil.
+— Cela fait combien de temps que vous êtes mariés ?
+— Quarante-sept ans dans deux mois.
+— C’est un bail !
+— Oui, j’ai dû souvent me cramponner.
 Leurs clapets se ferment. Je ne sais pas qui discute avec ma vélle et o m’tape sur la coloquinte. A-t-elle besoin, cette prune melée de raconter ma vie ! Moi aussi, je l’ai supportée mais heureusement, j’avais des compensations extérieures. La pov Nicole n’était pas une beauté et avec les années, elle a davantage ressemblé à un bonhomme Michelin qu’à une femme attirante. Il m’aurait fallu une mobylette pour en faire le tour ! Pas étonnant si j’allais voer ailleurs ! De plus, elle était bête comme trente six cochons mais, comme on dit, quand le vin est tiré, il faut le boire…</p><br>
 
 <div class="text-center mt-3">
